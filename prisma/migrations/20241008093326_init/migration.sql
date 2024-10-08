2f97548bdf64886ab1bf608a03d50d4019e5bbe7
Sanskar Chirania <sanskar125112@gmail.com>
2024-10-08 15:15:30 +0530

    refactor: migrate prisma

--- prisma/migrations/20241008093326_init/migration.sql
+++ prisma/migrations/20241008093326_init/migration.sql
@@ -1,26 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `post` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `profile` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `user` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropForeignKey
-ALTER TABLE `post` DROP FOREIGN KEY `Post_authorId_fkey`;
-
--- DropForeignKey
-ALTER TABLE `profile` DROP FOREIGN KEY `Profile_userId_fkey`;
-
--- DropTable
-DROP TABLE `post`;
-
--- DropTable
-DROP TABLE `profile`;
-
--- DropTable
-DROP TABLE `user`;
-
 -- CreateTable
 CREATE TABLE `audiotbl` (
     `Id` BIGINT NOT NULL AUTO_INCREMENT,
@@ -116,4 +93,14 @@ CREATE TABLE `postbl` (
     `ApiKey` VARCHAR(200) NULL,
 
     PRIMARY KEY (`Id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `User_username_key`(`username`),
+    PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
